bd56111b52a9ecb527af186b6c0e100e4038c8bf
JANIGEEK <josep_angelo@hotmail.com>
2024-03-29 23:01:28 -0600

    Creacion de paginas principales y header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-
+import React from "react";
+import "bootswatch/dist/darkly/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Details from "./pages/Details";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-  
   return (
-    <>
-      <div>
-        Hola mundo
-      </div>
-    </>
-  )
+    <Router>
+      <header>
+        <Header />
+      </header>
+
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="details/:id" element={<Details />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </main>
+
+      <footer></footer>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand mx-3" to="/">
+          Movie-Look
+        </Link>
+        <Link className="navbar-brand mx-3" to="/login">
+          Login
+        </Link>
+        <Link className="navbar-brand mx-3" to="/signup">
+          Signup
+        </Link>
+        {/*<Search/>*/}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
