51ea18f9034c9da5ce5abd6cf3c843e05e606ab0
JANIGEEK <josep_angelo@hotmail.com>
2024-03-30 22:56:56 -0600

    Formulario de login y register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [formData, setFormData] = useState({
+      email:"",
+      password:""
+    })
 
-export default Login
+    const { email, password} = formData
+
+    const onChange = (e) => {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.name]:e.target.value
+      }))
+    }
+
+    const onSubmit = (e) => {
+      e.preventDefault()
+    }
+
+  return (
+    <section className="container py-5">
+        <section className="container header">
+          <h4> Entrar a la App</h4>
+          <p>Por favor escribe tus credenciales</p>
+        </section>
+
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+              <input 
+                type="email"
+                className='form-control' 
+                id='email' 
+                name='email'
+                value={email}
+                placeholder="Ingresa tu email por favor"
+                onChange={onChange}
+              />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+              <input 
+                type="password"
+                className='form-control' 
+                id='password' 
+                name='password'
+                value={password}
+                placeholder="Ingresa tu password por favor"
+                onChange={onChange}
+              />
+          </div>
+
+          <button type="submit" className="btn btn-success my-3">Ingresar a la App</button>
+        </form>
+      </section>
+  );
+};
+
+export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  // informacion del ususario
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    password2: "",
+    isAdmin: false,
+  });
+
+  //Destructuracion de formData
+  const { first_name, last_name, email, password, password2, isAdmin } =
+    formData;
+
+  //Escritura de cada input
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  //Envio de formulario
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="container py-5">
+      <section className="container header">
+        <h4>Registrar nuevo usuario</h4>
+        <p>Por favor crea un nuevo usuario</p>
+      </section>
+
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label htmlFor="first_name" className="form-label">
+            Nombre
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="first_name"
+            name="first_name"
+            value={first_name}
+            placeholder="Ingresa tu nombre por favor"
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="last_name" className="form-label">
+            Apellidos
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="last_name"
+            name="last_name"
+            value={last_name}
+            placeholder="Ingresa tu apellido por favor"
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Ingresa tu email por favor"
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Ingresa tu password por favor"
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password2" className="form-label">
+            Confirmación de password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password2"
+            name="password2"
+            value={password2}
+            placeholder="Ingresa tu password otra vez"
+            onChange={onChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-success my-3">
+          Crear usuario
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Signup;
